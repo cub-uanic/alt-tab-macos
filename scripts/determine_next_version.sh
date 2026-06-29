2c706823c6e0159da5988dfe0d2a938cd13fe3eb
Oleg Kostyuk <cub.uanic@gmail.com>
2026-06-30 00:54:38 +0200

    feat: always PRO

--- scripts/determine_next_version.sh
+++ scripts/determine_next_version.sh
@@ -5,4 +5,9 @@ set -exu
 semanticRelease=$(npx semantic-release --dry-run --ci false)
 version=$(echo "$semanticRelease" | sed -nE 's/.+The next release version is (.+)/\1/p')
 
-echo "$version" > $VERSION_FILE
+if [ -z "$version" ]; then
+    echo "Error: could not determine next version from git tags via semantic-release" >&2
+    exit 1
+fi
+
+echo "$version" > "$VERSION_FILE"
